d177dc37656420e83658d3348e4a4c471349cbb0
mLordswe <alexander.csore@gmail.com>
2024-12-06 13:50:29 +0100

    baldersballe

--- src/func/dog.ts
+++ src/func/dog.ts
@@ -1,13 +1,20 @@
+import { dogs } from "../main";
 
-export const numberOfColors = (d: []) => {
-    let sumColors = d.length; // funkar sålänge det inte finns transparenta hundar
-    
-    
-    return sumColors
+type Color = "black" | "white" | "beige" | "gray" | "brown";
+export type Dog = {
+  name: string;
+  color: Color;
+};
+export const numberOfColors = (d: Dog[]): number => {
+  // => d.length;
+
+  let sumColors = d.length; // funkar sålänge det inte finns transparenta hundar
+
+  return sumColors;
 };
 
-export const commonColor = (d: []) => {
-    
-        
-    
+export const commonColor = (d: Dog[]) => {
+  //nedbrytning av problem
+  //få en lista av färgerna
+  //sök igenom listan och få fram färgen
 };
--- src/func/user-functions.ts
+++ src/func/user-functions.ts
@@ -1,15 +1,58 @@
-export const averageAge = (allUsers) => {
-	let allAges = 0;
-	allUsers.forEach((u) => {
-		allAges += u.age;
-	});
-	return allAges / allUsers.length;
+export type Person = {
+  name: string;
+  age: number;
+  hobby: string[];
+};
+export const averageAge = (allUsers: Person[]) => {
+  let allAges = 0;
+  allUsers.forEach((u) => {
+    allAges += u.age;
+  });
+  return allAges / allUsers.length;
 };
 
-export const averageNumberOfHobbies = (allUsers) => {
-	let AllHobbies = 0;
-	allUsers.forEach((u) => {
-		AllHobbies += u.hobby.length;
-	});
-	return AllHobbies / allUsers.length;
+export const averageNumberOfHobbies = (allUsers: Person[]) => {
+  let AllHobbies = 0;
+  allUsers.forEach((u) => {
+    AllHobbies += u.hobby.length;
+  });
+  return AllHobbies / allUsers.length;
 };
+
+export const oldestUser = (allUsers: Person[]) => {
+  let oldest;
+  let ages: number[] = [];
+  allUsers.forEach((u) => {
+    ages.push(u.age);
+  });
+  oldest = ages.sort(Math.max).reverse().pop();
+  return oldest;
+};
+export const oldestPerson = (allUsers: Person[]) => {
+  let oldest;
+  let ages: Person[] = [];
+  allUsers.forEach((u) => {
+    ages.push(u);
+  });
+  oldest = ages.sort().reverse().pop();
+
+  return console.log(
+    `den äldsta användaren heter ${oldest?.name} och är ${oldest?.age} år gammal`
+  );
+};
+
+//*************TODO få denna att fungera***************
+//*****************************************************
+// export const mostHobbies = (allUsers: Person[]) => {
+//   let hobbies: any;
+//   let hobbyamount: any[] = [];
+//   allUsers.forEach((u) => {
+//     hobbyamount.push(u.hobby);
+//     if (u.hobby.length > Math.max(u.hobby.length)) {
+//       hobbyamount.filter(String);
+//       console.log(hobbies.sort().pop());
+//     }
+//   });
+
+//   return console.log(hobbies);
+// };
